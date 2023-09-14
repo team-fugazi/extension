b8510b85796723ef5ab95abfdfca096825a31ea4
Frederik Bode <thorbensen@gmail.com>
2023-09-14 13:23:33 +0200

    feat(content_scripts): added a simple content script to project

---
+++ public/content.js
@@ -0,0 +1,16 @@
+// content.js
+
+// Function to change the background color of all paragraphs to yellow
+function changeParagraphColor() {
+  const paragraphs = document.querySelectorAll("p");
+  paragraphs.forEach((paragraph) => {
+    paragraph.style.backgroundColor = "yellow";
+  });
+}
+
+// Execute the function when the DOM is ready
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", changeParagraphColor);
+} else {
+  changeParagraphColor();
+}
