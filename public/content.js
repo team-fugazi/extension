a7190e88db031c7c64e5ce6e6832b6ddd7a69ac4
Jakup Karagoz <jakar19@student.sdu.dk>
2023-11-10 13:14:13 +0100

    Extension fix

--- public/content.js
+++ public/content.js
@@ -8,43 +8,58 @@ function changeParagraphColor() {
   });
 }
 
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-function getAllPosts() {
-  console.log("hello")
-  const posts = document.querySelectorAll("div[data-testid='post-container']")
+/*chrome.runtime.sendMessage("get-user-data", (response) => {
+  console.log(response);
+  console.log("hello");
+  getAllPosts();
+});
+*/
+
+// Run script again once dom changes
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("Hello");
+
+  return true;
+});
+async function getAllPosts() {
+  await delay(6000);
+
+  console.log("hello");
+  const posts = document.querySelectorAll("div[data-testid='post-container']");
   posts.forEach((post) => {
+    console.log("being called");
 
-    const title = post.querySelector("h3")
-    const paragraph = post.querySelectorAll("p")
-    //console.log(title)
-    //console.log(paragraph)
-    
-    
-    const postNodes = post.querySelectorAll("div:not(:has(> div))")
-    const lastNode = postNodes[postNodes.length - 1]
-    
-    var container = document.createElement("div")
-    var circle = document.createElement('div');
-    var statusContainer = document.createElement('div');
-    
-    container.className = "container"
-    circle.className = "circle"
-    statusContainer.className = "status-container"
+    const title = post.querySelector("h3");
+    const paragraph = post.querySelectorAll("p");
 
-    
-    
-    lastNode.appendChild(container)
-    container.appendChild(circle)
-    
-    circle.appendChild(statusContainer)
+    const postNodes = post.querySelectorAll("div:not(:has(> div))");
+    const lastNode = postNodes[postNodes.length - 1];
 
+    var container = document.createElement("div");
+    var circle = document.createElement("div");
+    var statusContainer = document.createElement("div");
+
+    container.className = "container";
+    circle.className = "circle";
+    statusContainer.className = "status-container";
+
+    lastNode.appendChild(container);
+    container.appendChild(circle);
+
+    circle.appendChild(statusContainer);
   });
 }
 
 // Execute the function when the DOM is ready
 if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", changeParagraphColor);
+  //getAllPosts()
+  //document.addEventListener("DOMContentLoaded", changeParagraphColor);
+  document.addEventListener("DOMContentLoaded", getAllPosts, false);
 } else {
   //changeParagraphColor();
-  getAllPosts()
+  //document.addEventListener("load", getAllPosts, false);
+  getAllPosts();
 }
