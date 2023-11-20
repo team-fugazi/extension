ee70ef62d4213c26dee55ff5d4bbb09c9daeeb98
Jakup Karagoz <jakar19@student.sdu.dk>
2023-11-20 14:32:27 +0100

    Changed loginbutton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 // Path: src/App.tsx
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 
 // Local Components
 import MainScreen from "./pages/Main";
+import { useEffect } from "react";
 
 // Environment variables
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 function App() {
+  const { isAuthenticated, getAccessTokenSilently, isLoading } = useAuth0();
+
+  // const getToken = async () => {
+  //   try {
+  //     const token = await getAccessTokenSilently();
+  //     setToken(token);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getToken();
+  // }, []);
+
   return (
     <Auth0Provider
       domain={domain}
--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,17 +4,18 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const LoginButton = () => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, loginWithPopup } = useAuth0();
 
   return (
     !isAuthenticated && (
       <button
         onClick={() => {
-          loginWithRedirect({
-            async openUrl(url) {
-              await chrome.tabs.create({ url: url }); // todo: ignore chrome object in ts config
-            },
-          });
+          // loginWithRedirect({
+          //   async openUrl(url) {
+          //     await chrome.tabs.create({ url: url }); // todo: ignore chrome object in ts config
+          //   },
+          // });
+          loginWithPopup();
         }}
       >
         Log in
