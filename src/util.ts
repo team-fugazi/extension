881dc2892d5a4d473bc205af22e2e647556a1960
Jakup Karagoz <jakar19@student.sdu.dk>
2023-11-21 14:08:30 +0100

    util removed

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-function getSubredditName() {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-    console.log(url);
-  });
-}
