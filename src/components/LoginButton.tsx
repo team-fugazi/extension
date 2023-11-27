2941ddc9a54b0659e29232f6ae0b3198e5a2c35a
chakup <cavras2009@hotmail.com>
2023-11-27 21:09:56 +0100

    changes

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,13 +4,25 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const LoginButton = () => {
-  const { isAuthenticated, loginWithPopup } = useAuth0();
+  const { isAuthenticated, loginWithPopup, getAccessTokenSilently } =
+    useAuth0();
+
+  const handleLogin = async () => {
+    loginWithPopup()
+      .then((result) => {
+        console.log(result);
+        const accessToken = getAccessTokenSilently();
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  };
 
   return (
     !isAuthenticated && (
       <button
         onClick={() => {
-          loginWithPopup();
+          handleLogin();
         }}
       >
         Log in
